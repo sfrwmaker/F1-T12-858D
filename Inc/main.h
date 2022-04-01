5f08b99430cdc1074ef8388510fbb7d5460cfed0
sfrwmaker <sfrwmaker@gmail.com>
2022-04-01 20:37:44 +0300

    Add files via upload

--- Inc/main.h
+++ Inc/main.h
@@ -104,7 +104,7 @@ void Error_Handler(void);
 #define BUZZER_Pin GPIO_PIN_9
 #define BUZZER_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
-#define FW_VERSION	("4.06")
+#define FW_VERSION	("4.07")
 
 /* USER CODE END Private defines */
 
